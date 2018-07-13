33b37f3d5e49e5942d4d97de63a84ea4a602c99d
Nekliukov <roman.nekliukov@gmail.com>
2018-07-13 13:52:56 +0300

    2 of 4 pass maze tests done

--- MazeLibrary.Tests/MazeSolverTests.cs
+++ MazeLibrary.Tests/MazeSolverTests.cs
@@ -6,9 +6,9 @@ namespace MazeLibrary.Tests
     [TestFixture]
     public class MazeSolverTests
     {
-        private readonly int[] startXs = { 1, 0, 3, 0 };
+        private readonly int[] startXs = { 3, 0, 1, 0 };
 
-        private readonly int[] startYs = { 0, 1, 5, 4 };
+        private readonly int[] startYs = { 5, 4, 0, 1 };
 
         private readonly int[][,] sourceData = new int[][,]
         {
@@ -66,7 +66,7 @@ namespace MazeLibrary.Tests
         {
             new int[,]
             {
-                { -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1 },  
                 { 10,  9,  0, -1,  0, -1 },
                 { -1,  8, -1, -1,  0, -1 },
                 { -1,  7, -1,  3,  2,  1 },
@@ -130,19 +130,33 @@ namespace MazeLibrary.Tests
         public void PassMaze_SuccessfulTests()
         {
             for (int i = 0; i < sourceData.Length; i++)
-            {
+            {               
                 MazeSolver solver = new MazeSolver(sourceData[i], startXs[i], startYs[i]);
 
                 solver.PassMaze();
-
+                CollectionAssert.AreEqual(solver.MazeWithPass(), result[i]);
                 if (!MatrixAreEquals(solver.MazeWithPass(), result[i]))
                 {
-                    //TODO
+                    throw new ArgumentException("Matrix are not equal!");
                 }
             }
         }
 
-        private static bool MatrixAreEquals(int[,] lhs, int[,] rhs) => throw new NotImplementedException();
+        private static bool MatrixAreEquals(int[,] lhs, int[,] rhs)
+        {
+            for (int i = 0; i < lhs.GetLength(0); i++)
+            {
+                for (int j = 0; j < lhs.GetLength(0); j++)
+                {
+                    if (lhs[i,j] != rhs[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
 
     }
 }
--- MazeLibrary/MazeSolver.cs
+++ MazeLibrary/MazeSolver.cs
@@ -4,10 +4,153 @@ namespace MazeLibrary
 {
     public class MazeSolver
     {
-        public MazeSolver(int[,] mazeModel, int startX, int startY) => throw new NotImplementedException();
+        #region Private fields
+        private readonly int startX, startY;
+        private int[,] maze;
 
-        public int[,] MazeWithPass() => throw new NotImplementedException();
+        private int step = 1;
+        private bool finish = false;
+        #endregion
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MazeSolver"/> class.
+        /// </summary>
+        /// <param name="mazeModel">The maze model.</param>
+        /// <param name="startX">The start x.</param>
+        /// <param name="startY">The start y.</param>
+        /// <exception cref="ArgumentNullException">Null array argument was sent</exception>
+        /// <exception cref="ArgumentException">Index out of range</exception>
+        /// <exception cref="ArgumentException">There is no finish point in maze</exception>
+        public MazeSolver(int[,] mazeModel, int startX, int startY)
+        {
+            if (mazeModel == null)
+            {
+                throw new ArgumentNullException($"Null array argument was sent");
+            }
 
-        public void PassMaze() => throw new NotImplementedException();
+            if (startX >= mazeModel.GetLength(0) || startX < 0)
+            {
+                throw new ArgumentException($"Index {startX} out of range");
+
+            }
+
+            if (startY >= mazeModel.GetLength(1) || startY < 0)
+            {
+                throw new ArgumentException($"Index {startY} out of range");
+            }
+
+            if (!IsFinishExists(mazeModel))
+            {
+                throw new ArgumentException("There is no finish point in maze");
+            }
+
+            this.startX = startX;
+            this.startY = startY;
+            maze = new int[mazeModel.GetLength(0), mazeModel.GetLength(1)];
+            Copy2dArray(mazeModel, maze);
+        }
+
+
+        public int[,] MazeWithPass() { return maze; }
+
+        /// <summary>
+        /// Passes the maze to find finish.
+        /// </summary>
+        public void PassMaze()
+        {
+            MakeStep(startX, startY, 0);
+            ChangeToZero();
+        }
+
+        /// <summary>
+        /// Recursive method to make every step
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="finishSide">The finish side.</param>
+        private void MakeStep(int x, int y, int finishSide)
+        {
+            if (y == finishSide && maze[x, y] == 0)
+            {
+                // finish has been founded
+                maze[x, y] = step++;
+                finish = true;
+            }
+
+            if (x != maze.GetLength(0) - 1 && maze[x + 1, y] == 0)
+            {
+                maze[x, y] = step++;
+                MakeStep(x + 1, y, finishSide);
+            }
+
+            if (x != 0 && maze[x - 1, y] == 0)
+            {
+                maze[x, y] = step++;
+                MakeStep(x - 1, y, finishSide);
+            }
+
+            if (y + 1 != maze.GetLength(1) && maze[x, y + 1] == 0)
+            {
+                maze[x, y] = step++;
+                MakeStep(x, y + 1, finishSide);
+            }
+
+            if (y != 0 && maze[x, y - 1] == 0)
+            {
+                maze[x, y] = step++;
+                MakeStep(x, y - 1, finishSide);
+            }
+
+            step -= 1;
+            if (!finish)
+            {
+                maze[x, y] = -2;
+            }
+        }
+
+        private void ChangeToZero()
+        {
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    if (maze[i, j] == -2)
+                    {
+                        maze[i, j] = 0;
+                    }
+                }
+            }
+        }
+
+        private bool IsFinishExists(int[,] mazeModel)
+        {
+            for (int i = 0; i < mazeModel.GetLength(0); i++)
+            {
+                if (mazeModel[i, 0] == 0 || mazeModel[i, mazeModel.GetLength(1) - 1] == 0)
+                {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < mazeModel.GetLength(1); i++)
+            {
+                if (mazeModel[0, i] == 0 || mazeModel[mazeModel.GetLength(0) - 1, i] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void Copy2dArray(int[,] from, int[,] to)
+        {
+            for (int i = 0; i < from.GetLength(0); i++)
+            {
+                for (int j = 0; j < from.GetLength(1); j++)
+                {
+                    to[i, j] = from[i, j];
+                }
+            }
+        }
     }
 }
